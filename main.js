df83ad20f15c380fde86c0bceeece7f7381b3957
Thiago Brito <deoliveira.tbrito@gmail.com>
2023-06-19 23:31:31 -0300

    formulario HTML 1

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form-comparador")
+
+
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const numA = parseFloat (document.getElementById('campoa').value);
+    const numB = parseFloat (document.getElementById('campob').value);
+    const mensagemcompare = 'O campo B: ' + numB + ' é maior que o campo A: ' + numA;
+    const mensagemcompareN = 'O campo B: ' + numB + ' não é maior que o campo A: ' + numA;
+
+    function numerovalido(numA, numB){
+    if (numB > numA){
+        const containermensagem = document.querySelector('.mensagem')
+        containermensagem.innerHTML =  mensagemcompare;
+        containermensagem.style.display = 'block';;
+        return true;
+    } else {
+        const containermensagem = document.querySelector('.mensagem')
+        containermensagem.innerHTML =  mensagemcompareN;
+        containermensagem.style.display = 'block';;
+        return false;
+    }
+}
+formevalido = numerovalido(numA, numB)
+});
+
