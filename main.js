953b6f1c05292785e3b2218dc13f7723c7f106b2
Thiago Brito <deoliveira.tbrito@gmail.com>
2023-06-19 23:37:40 -0300

    atualizaço do js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ form.addEventListener("submit", function(e) {
 
     const numA = parseFloat (document.getElementById('campoa').value);
     const numB = parseFloat (document.getElementById('campob').value);
-    const mensagemcompare = 'O campo B: ' + numB + ' é maior que o campo A: ' + numA;
-    const mensagemcompareN = 'O campo B: ' + numB + ' não é maior que o campo A: ' + numA;
+    const mensagemcompare = 'Formulario valido: o campo B: ' + numB + ' é maior que o campo A: ' + numA;
+    const mensagemcompareN = 'Formulario Invalido: o campo B: ' + numB + ' não é maior que o campo A: ' + numA;
 
     function numerovalido(numA, numB){
     if (numB > numA){
